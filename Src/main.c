efcf769ba819694f3b1471fd0fdb62b80a780164
Mikhail Tsaryov <michael.tsaryov@gmail.com>
2020-03-28 22:33:50 +0400

    No comments

--- Src/main.c
+++ Src/main.c
@@ -159,8 +159,21 @@ int main(void)
 //   while (ExternalFlash_ReadStatusRegister() & BUSY);
 //   ExternalFlash_WriteByte(&ActiveInput, &SaveStartAddr);
 
-//   sprintf(UART_BufSrting, "Read after write:  %X\r\n", ExternalFlash_Read(&SaveStartAddr));
-//   HAL_UART_Transmit(&huart1, (uint8_t *)UART_BufSrting, strlen(UART_BufSrting), HAL_MAX_DELAY);
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        sprintf(UART_BufSrting, "< %X\r\n", ExternalFlash_Read(&SaveStartAddr + i));
+        HAL_UART_Transmit(&huart1, (uint8_t *)UART_BufSrting, strlen(UART_BufSrting), HAL_MAX_DELAY);
+    }
+
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        while (ExternalFlash_ReadStatusRegister() & BUSY);
+        ExternalFlash_WriteByte(&i, &SaveStartAddr + i);
+        while (ExternalFlash_ReadStatusRegister() & BUSY);
+        sprintf(UART_BufSrting, "> %X\r\n", ExternalFlash_Read(&SaveStartAddr + i));
+        HAL_UART_Transmit(&huart1, (uint8_t *)UART_BufSrting, strlen(UART_BufSrting), HAL_MAX_DELAY);
+    }
+   
 
   // OLED Init
   disp1color_Init();
@@ -228,7 +241,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -238,16 +251,16 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -593,7 +606,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     // Если не запущена обработка другой кнопки
     if ((GPIO_Pin == BTN_PWR_Pin) | (GPIO_Pin == BTN_IN_Pin) | (GPIO_Pin == BTN_OUT_Pin))
     {
-        DeBouncer_Timer = DEBOUCE_TIME; // Инициализируем таймер
+        DeBouncer_Timer = DEBOUCE_TIME; // �?нициализируем таймер
         DeBouncer_Task = TRUE;          // Взводим флаг задачи подавления дребезга
     }
 }
